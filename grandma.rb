cac37e8718bca3069e00812b4cd113bf33523f3b
Jane Justiz <janejustiz@gmail.com>
2018-06-13 02:40:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(speak)
-  if speak == speak.upcase
-    return "NO, NOT SINCE 1938!"
-  elsif speak == "I LOVE YOU GRANDMA!"
+  if speak == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif speak == speak.upcase
+    return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
   end
